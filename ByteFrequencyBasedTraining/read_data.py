601e31a6324e3cfb45a23dfebcec03c1e76f60ab
Jonghoon Won <baryberri@icloud.com>
2017-09-06 22:12:21 +0900

    Update read_data.py to choose between train and test data

--- ByteFrequencyBasedTraining/read_data.py
+++ ByteFrequencyBasedTraining/read_data.py
@@ -3,23 +3,29 @@ import tensorflow as tf
 import unittest
 
 
-def filename_list_in_data_directory():
+def filename_list_in_data_directory(path_dir: str):
     """
     read a directory and returns a list of included file paths.
+    :param path_dir: path's directory to read
     :return: filename's list in the data directory
     """
-    path_dir = "frequency_data"
     filename_list = os.listdir(path_dir)
     return filename_list
 
 
-def read_data():
+def read_data(data_type: str):
     """
     read the data folder and returns a single csv file.
+    :param data_type: "train" or "test"
     :return: a csv file's parsed result
     """
     try:
-        filename_list = filename_list_in_data_directory()
+        if data_type.lower() == "train":
+            filename_list = filename_list_in_data_directory("./train_data")
+        elif data_type.lower() == "test":
+            filename_list = filename_list_in_data_directory("./test_data")
+        else:
+            raise ValueError("only train or test data is available.")
         filename_queue = tf.train.string_input_producer(filename_list, shuffle=True)
         reader = tf.TextLineReader()
         key, value = reader.read(filename_queue)
@@ -31,13 +37,14 @@ def read_data():
         exit()
 
 
-def read_data_batch(batch_size):
+def read_data_batch(data_type: str, batch_size: int):
     """
     read a data file, and returns batch data.
+    :param data_type: "train" or "test"
     :param batch_size: bach size to get
     :return: batch value and file type encoded by one-hot encoding
     """
-    input_data = read_data()
+    input_data = read_data(data_type)
     byte_value = input_data[:][0:256]
     file_type = input_data[:][256:]
 
