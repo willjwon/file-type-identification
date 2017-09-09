70a5e1eb27d6c0c9eefc72436d5e9c771ccdd15f
Jonghoon Won <baryberri@icloud.com>
2017-09-09 15:00:41 +0900

    Update setup.py

--- ByteFrequencyBasedTraining/setup.py
+++ ByteFrequencyBasedTraining/setup.py
@@ -20,27 +20,27 @@ FLAGS.train_data_path = "./train_data"
 FLAGS.validation_data_path = "./validation_data"
 FLAGS.test_data_path = "./test_data"
 
-# learning rate
-FLAGS.learning_rate = 1e-4
-
-# keep probability for dropout while training
-keep_prob_train = 0.7
-
-# number of epochs to repeat training
-FLAGS.num_of_epochs = 15
-
-# a mini batch's size
-FLAGS.batch_size = 10
-
-# total data size
-FLAGS.num_of_total_fragments = 500
-
-# csv information
-FLAGS.num_of_fragments_per_csv = 3
-
-# keep probability for dropout while training
-keep_prob_train = 0.7
-
 # directory for tensorboard summary
 FLAGS.tensorboard_directory = './tensorboard/train1'
+
+# model;s path
+FLAGS.model_path = "./model/model.ckpt"
+
+# learning rate
+FLAGS.learning_rate = 1e-5
+
+# keep probability for dropout while training
+FLAGS.keep_prob_train = 0.7
+
+# global steps to repeat
+FLAGS.num_of_total_global_step = 20000
+
+# checkpoint steps to save and validate
+FLAGS.checkpoint_steps = 50
+
+# a mini batch's size
+FLAGS.batch_size = 50
+
+# csv information
+FLAGS.num_of_fragments_per_csv = 100
 # =================================
