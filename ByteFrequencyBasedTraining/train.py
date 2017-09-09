8787d7c0c7f721fd0e4849e0c5c842ba9ea55fb7
Jonghoon Won <baryberri@icloud.com>
2017-09-09 15:16:38 +0900

    Implement train.py to save and restore, and make .gitignore to dismiss tensorflow input files

--- ByteFrequencyBasedTraining/train.py
+++ ByteFrequencyBasedTraining/train.py
@@ -1,57 +1,67 @@
-from setup import *
 from read_data import *
 from neural_net import *
 
 
 def main():
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-        logits=hypothesis, labels=Y))
+    train_queue, validation_queue, test_queue = make_files_queue()
+    train_batch_byte_value, train_batch_file_type = next_train_batch(batch_size, train_queue)
+    validation_batch_byte_value, validation_batch_file_type = validation_data_set(validation_queue)
+    test_batch_byte_value, test_batch_file_type = test_data_set(test_queue)
 
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+    global_step = tf.Variable(0, trainable=False, name="global_step")
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1)), tf.float32))
+    optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate).minimize(cost, global_step=global_step)
     tf.summary.scalar(name="cost", tensor=cost)
 
-    train_batch_byte_value, train_batch_file_type = next_train_batch("train", batch_size)
+    saver = tf.train.Saver(max_to_keep=5)
 
     # tensorboard summary
     summary = tf.summary.merge_all()
     writer = tf.summary.FileWriter(tensorboard_directory)
 
-    global_step = 0
-
     with tf.Session() as sess:
         writer.add_graph(sess.graph)
-        sess.run(tf.global_variables_initializer())
+
+        checkpoint = tf.train.get_checkpoint_state(FLAGS.model_path)
+        if checkpoint and checkpoint.model_checkpoint_path:
+            saver.restore(sess, checkpoint.model_checkpoint_path)
+        else:
+            sess.run(tf.global_variables_initializer())
 
         # set the threads for reading data
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-        # train the mo/del
-        for epoch in range(num_of_epochs):
-            avg_cost = 0
+        current_step = sess.run(global_step)
 
-            for batch in range(batch_per_epoch):
-                batch_byte_value, batch_file_type = sess.run([train_batch_byte_value, train_batch_file_type])
-                feed_dict = {X: batch_byte_value, Y: batch_file_type, keep_prob: 0.7}
-                c, _, s = sess.run([cost, optimizer, summary], feed_dict=feed_dict)
-                avg_cost += c / batch_per_epoch
+        # train the model
+        for step in range(current_step, FLAGS.num_of_total_global_steps):
+            # validation and save at checkpoint
+            if step % FLAGS.checkpoint_steps == 0:
+                validation_byte_value, validation_file_type \
+                    = sess.run([validation_batch_byte_value, validation_batch_file_type])
+                computed_accuracy = \
+                    sess.run(accuracy, feed_dict={X: validation_byte_value, Y: validation_file_type, keep_prob: 1.0})
+                print("\tAt step {:>5}, accuracy: {:2.9f}".format(step, computed_accuracy))
+                saver.save(sess, FLAGS.model_path, global_step=step)
 
-                # add summary to tensorboard
-                writer.add_summary(s, global_step=global_step)
-                global_step += 1
+            # train step
+            batch_byte_value, batch_file_type = sess.run([train_batch_byte_value, train_batch_file_type])
+            cost, _, summary = sess.run([cost, optimizer, summary],
+                                        feed_dict={X: batch_byte_value,
+                                                   Y: batch_file_type,
+                                                   keep_prob: FLAGS.keep_prob_train})
 
-            print("Epoch:", "{:0>4}".format(epoch + 1), "cost:", "{:.9f}".format(avg_cost))
+            # add summary to tensorboard
+            writer.add_summary(summary, global_step=global_step)
 
+            print("step {:>5}, cost: {:2.9f}".format(step, cost))
 
-        # test.csv the model
-        # TODO: modify read_data_batch for testing (batch_size)
-        test_x, test_y = next_train_batch("test.csv", 1000)
-
-        correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-        feed_dict = {X: test_x, Y: test_y, keep_prob: 1}
-        print("Accuracy:", sess.run(accuracy, feed_dict=feed_dict))
+        # test the model
+        test_byte_value, test_file_type = sess.run([test_batch_byte_value, test_batch_file_type])
+        computed_accuracy = sess.run(accuracy, feed_dict={X: test_byte_value, Y: test_file_type, keep_prob: 1.0})
+        print("=== Test Result, accuracy: {:2.9f}".format(computed_accuracy))
 
         coord.request_stop()
         coord.join(threads)
