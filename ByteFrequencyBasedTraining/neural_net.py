054c304fbeb836aa905f9a4889c936755c21d7a3
Jonghoon Won <baryberri@icloud.com>
2017-09-18 22:56:14 +0900

    Refactor neural_net.py using layer.py

--- ByteFrequencyBasedTraining/neural_net.py
+++ ByteFrequencyBasedTraining/neural_net.py
@@ -1,3 +1,4 @@
+from layer import *
 from setup import *
 
 # input placeholders
@@ -9,53 +10,8 @@ Y = tf.placeholder(tf.float32, [None, FLAGS.num_of_file_types])
 # keep probability placeholder for dropout
 keep_prob = tf.placeholder(tf.float32)
 
-# weights & bias
-with tf.variable_scope("layer1"):
-    W1 = tf.get_variable(shape=[256, 512], initializer=tf.contrib.layers.xavier_initializer(), name="W1")
-    b1 = tf.Variable(tf.random_normal([512]), name="b1")
-    L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
-    L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
-
-    tf.summary.histogram("X", X)
-    tf.summary.histogram("weights", W1)
-    tf.summary.histogram("bias", b1)
-    tf.summary.histogram("layer", L1)
-
-with tf.variable_scope("layer2"):
-    W2 = tf.get_variable(shape=[512, 512], initializer=tf.contrib.layers.xavier_initializer(), name="W2")
-    b2 = tf.Variable(tf.random_normal([512]), name="b2")
-    L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
-    L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
-
-    tf.summary.histogram("weights", W2)
-    tf.summary.histogram("bias", b2)
-    tf.summary.histogram("layer", L2)
-
-with tf.variable_scope("layer3"):
-    W3 = tf.get_variable(shape=[512, 512], initializer=tf.contrib.layers.xavier_initializer(), name="W3")
-    b3 = tf.Variable(tf.random_normal([512]), name="b3")
-    L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
-    L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
-
-    tf.summary.histogram("weights", W3)
-    tf.summary.histogram("bias", b3)
-    tf.summary.histogram("layer", L3)
-
-with tf.variable_scope("layer4"):
-    W4 = tf.get_variable(shape=[512, 512], initializer=tf.contrib.layers.xavier_initializer(), name="W4")
-    b4 = tf.Variable(tf.random_normal([512]), name="b4")
-    L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
-    L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
-
-    tf.summary.histogram("weights", W4)
-    tf.summary.histogram("bias", b4)
-    tf.summary.histogram("layer", L4)
-
-with tf.variable_scope("layer5"):
-    W5 = tf.get_variable(shape=[512, FLAGS.num_of_file_types], initializer=tf.contrib.layers.xavier_initializer(), name="W5")
-    b5 = tf.Variable(tf.random_normal([FLAGS.num_of_file_types]), name="b5")
-    hypothesis = tf.matmul(L4, W5) + b5
-
-    tf.summary.histogram("weights", W5)
-    tf.summary.histogram("bias", b5)
-    tf.summary.histogram("hypothesis", hypothesis)
+L1 = layer("1", input_tensor=X, input_size=256, output_size=512, relu=True, dropout=True, keep_prob=keep_prob)
+L2 = layer("2", input_tensor=L1, input_size=512, output_size=256, relu=True, dropout=True, keep_prob=keep_prob)
+L3 = layer("3", input_tensor=L2, input_size=256, output_size=128, relu=True, dropout=True, keep_prob=keep_prob)
+L4 = layer("4", input_tensor=L3, input_size=128, output_size=32, relu=True, dropout=True, keep_prob=keep_prob)
+hypothesis = layer("5", input_tensor=L4, input_size=32, output_size=5, relu=False, dropout=False, keep_prob=keep_prob)
