32511c928aa81f72929b61580dc86e0358d74b05
Jonghoon Won <baryberri@icloud.com>
2017-09-24 17:24:02 +0900

    Add logic not to download similiar html pages

--- FileScraper/FileScraper/spiders/FileScraper.py
+++ FileScraper/FileScraper/spiders/FileScraper.py
@@ -1,5 +1,6 @@
 import os
 import scrapy
+from FileScraper.hashchecker import *
 from urllib.parse import urljoin
 from scrapy.spiders import CrawlSpider
 
@@ -7,11 +8,12 @@ from scrapy.spiders import CrawlSpider
 class FileScraper(CrawlSpider):
     name = "file-scraper"
 
-    start_urls = ["http://admission.kaist.ac.kr/undergraduate/"]
-    allowed_domains = ["admission.kaist.ac.kr"]
+    start_urls = ["http://cse.snu.ac.kr"]
 
     files_to_download = (".hwp", ".pdf", ".docx", ".xlsx", ".exe", ".mp3")
     images_to_download = (".jpg", ".png", ".gif")
+
+    hash_checker = HashChecker()
 
     def parse(self, response):
         # check the given request is valid html document type.
@@ -19,14 +21,24 @@ class FileScraper(CrawlSpider):
             return
 
         # save response page
-        html_save_link = "./html/" + \
-                         response.url.strip().split("//")[1].replace("/", "#") + \
-                         ".html"
-        try:
-            with open(html_save_link, "wb") as html_file:
-                html_file.write(response.body)
-        except FileNotFoundError:
-            os.makedirs("./html/")
+        html_name = response.url.strip().split("//")[1].replace("/", "#")
+        html_name_split = html_name.split("#")
+        if len(html_name_split) == 1 or len(html_name_split) == 2:
+            url_hash_seed = html_name
+        else:
+            try:
+                url_hash_seed = html_name_split[0] + html_name_split[1] + html_name_split[3][:3]
+            except IndexError:
+                # html_name_split has less than 5 characters. use the base seed.
+                url_hash_seed = html_name_split[0] + html_name_split[1]
+
+        if not self.hash_checker.duplicated(url_hash_seed):
+            html_save_link = "./html/" + html_name + ".html"
+            try:
+                with open(html_save_link, "wb") as html_file:
+                    html_file.write(response.body)
+            except FileNotFoundError:
+                os.makedirs("./html/")
 
         # download images
         image_links = list(set(response.xpath("//img/@src").extract()))  # list(set()) to remove duplicate links
