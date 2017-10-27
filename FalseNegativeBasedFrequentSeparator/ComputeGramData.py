62445efe35dbc9ba3a0db0fc070f3ea28650ca04
Jonghoon Won <baryberri@icloud.com>
2017-10-27 18:47:20 +0900

    Implement FalseNegativeBasedSeparatorCounter

---
+++ FalseNegativeBasedFrequentSeparator/ComputeGramData.py
@@ -0,0 +1,33 @@
+import numpy
+import settings
+
+
+def count_gram_frequency(gram_size, read_data, gram_frequency, introduced_grams):
+    grams_introduced_in_this_fragment = set()
+    for index in range(len(read_data) - gram_size + 1):
+        gram_value = int.from_bytes(read_data[index:(index + gram_size)], byteorder="big")
+        gram_key = (gram_size, gram_value)
+        if gram_key not in grams_introduced_in_this_fragment:
+            grams_introduced_in_this_fragment.add(gram_key)
+            if gram_key in gram_frequency:
+                gram_frequency[gram_key] += 1
+            else:
+                gram_frequency[gram_key] = 1
+
+        introduced_grams.add(gram_key)
+
+
+def compute_gram_variance(gram_key, gram_frequency_data_by_types):
+    data = []
+    for file_type in settings.directory_path.keys():
+        frequency_dict = gram_frequency_data_by_types[file_type]
+
+        if gram_key in frequency_dict.keys():
+            data.append(frequency_dict[gram_key])
+        else:
+            data.append(0)
+
+    num_max_fragments = numpy.max(data)
+    num_sum_fragments = numpy.sum(data)
+    num_total_fragments = settings.num_fragments * len(settings.directory_path)
+    return (settings.num_fragments - num_max_fragments) / (num_total_fragments - num_sum_fragments)
