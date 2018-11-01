94ef09619a1a0ccd4d1c7ce0e791fbfe73162efb
Jonghoon Won <baryberri@icloud.com>
2018-11-01 14:40:39 +0900

    Update to gram-based frequency filter

--- frequent_separator_checker/main.py
+++ frequent_separator_checker/main.py
@@ -10,8 +10,11 @@ def main():
     separator_checker = SeparatorChecker()
     obvious_separators, obvious_gram_size = SeparatorChecker.obvious_separators()
     candidate_separators, gram_size = SeparatorChecker.init_candidate()
+    total_frequent_separators = dict()
 
     # Compute
+    print("At 2-gram:")
+    print("\tCandidate: 65536 separators.")
     while len(candidate_separators) != 0:
         separator_checker.set_candidate_separators(candidate_separators=candidate_separators, gram_size=gram_size)
         fragment = fragment_getter.get_fragment()
@@ -19,13 +22,36 @@ def main():
             separator_checker.count_fragment(fragment=fragment)
             fragment = fragment_getter.get_fragment()
 
-        frequent_separators = separator_checker.get_frequent_separators(proportion=0.01)
-        print("At {}-gram, {} separators are frequent.".format(gram_size, len(frequent_separators)))
+        # frequent_separators_list = separator_checker.get_frequent_separators_absolute(top_n=10000)
+        # frequent_separators_list = SeparatorChecker.filter_by_frequency(frequent_separators_list, threshold=0.05)
+        frequent_separators_list = separator_checker.get_frequent_separators_frequency(threshold=0.05)
 
-        candidate_separators = separator_checker.make_candidate_separators(frequent_separators)
+        print("\t{} separators are frequent.".format(len(frequent_separators_list)))
+
+        if len(frequent_separators_list) <= 0:
+            break
+
+        SeparatorChecker.print_top_5(frequent_separators_list)
+        SeparatorChecker.print_bottom_5(frequent_separators_list)
+
+        frequent_separators_set = SeparatorChecker.separators_into_set_form(frequent_separators_list)
+        total_frequent_separators[gram_size] = frequent_separators_set
+
+        candidate_separators = separator_checker.make_candidate_separators(frequent_separators_set)
         gram_size += 1
-        print("{}-gram candidates: {} separators.".format(gram_size, len(candidate_separators)))
-        print()
+
+        if gram_size > 20:
+            break
+
+        print("\nAt {}-gram:".format(gram_size))
+        print("\tCandidates: {} separators.".format(len(candidate_separators)))
+
+    print()
+    total_separators_count = 0
+    for gram, separators in total_frequent_separators.items():
+        print("At {}-gram, {} separators are selected.".format(gram, len(separators)))
+        total_separators_count += len(separators)
+    print("Total {} separators are selected.".format(total_separators_count))
 
 
 if __name__ == "__main__":
--- frequent_separator_checker/separator_checker.py
+++ frequent_separator_checker/separator_checker.py
@@ -5,7 +5,7 @@ class SeparatorChecker:
     def __init__(self):
         self.frequency_dict = None
         self.gram_size = 0
-        self.total_grams = 0
+        self.total_fragments_count = 0
 
     @staticmethod
     def obvious_separators():
@@ -24,25 +24,75 @@ class SeparatorChecker:
         for separator in candidate_separators:
             self.frequency_dict[separator] = 0
         self.gram_size = gram_size
-        self.total_grams = 0
+        self.total_fragments_count = 0
 
     def count_fragment(self, fragment):
+        gram_set = set()
         for i in range(len(fragment) - self.gram_size + 1):
-            gram = fragment[i:i + self.gram_size]
+            gram_set.add(fragment[i:i + self.gram_size])
+        for gram in gram_set:
             if gram in self.frequency_dict.keys():
                 self.frequency_dict[gram] += 1
-            self.total_grams += 1
+        self.total_fragments_count += 1
 
-    def get_frequent_separators(self, proportion):
-        frequent_separators = set()
+    @staticmethod
+    def separators_into_set_form(frequent_separators_list):
+        frequent_separators_set = set()
+        for separator in frequent_separators_list:
+            frequent_separators_set.add(separator[0])
+        return frequent_separators_set
+
+    def get_frequent_separators_relative(self, proportion):
         frequency_dict_sorted = sorted(self.frequency_dict.items(), key=operator.itemgetter(1), reverse=True)
-        print("top-5 frequent: {} (of {})".format(frequency_dict_sorted[:5], self.total_grams))
+        frequency_dict_sorted = list(map(lambda gram: (gram[0], gram[1] / self.total_fragments_count),
+                                         frequency_dict_sorted))
 
         num_frequent_separators = int(len(frequency_dict_sorted) * proportion)
-        for i in range(num_frequent_separators):
-            frequent_separators.add(frequency_dict_sorted[i][0])
+        return frequency_dict_sorted[:num_frequent_separators]
 
-        return frequent_separators
+    def get_frequent_separators_absolute(self, top_n):
+        frequency_dict_sorted = sorted(self.frequency_dict.items(), key=operator.itemgetter(1), reverse=True)
+        frequency_dict_sorted = list(map(lambda gram: (gram[0], gram[1] / self.total_fragments_count),
+                                         frequency_dict_sorted))
+
+        if len(frequency_dict_sorted) <= top_n:
+            return frequency_dict_sorted
+
+        return frequency_dict_sorted[:top_n]
+
+    def get_frequent_separators_frequency(self, threshold):
+        frequency_dict_sorted = sorted(self.frequency_dict.items(), key=operator.itemgetter(1), reverse=True)
+        frequency_dict_sorted = list(map(lambda gram: (gram[0], gram[1] / self.total_fragments_count),
+                                         frequency_dict_sorted))
+
+        i = 0
+        for i in range(len(frequency_dict_sorted)):
+            if frequency_dict_sorted[i][1] < threshold:
+                break
+
+        return frequency_dict_sorted[:i]
+
+    @staticmethod
+    def print_top_5(frequent_separators_list):
+        print_string = []
+        counts = min(5, len(frequent_separators_list))
+        for i in range(counts):
+            print_string.append("{}({:.2f}%)".format(frequent_separators_list[i][0],
+                                                     frequent_separators_list[i][1] * 100))
+        print("\tTop {}: {}".format(counts, ", ".join(print_string)))
+
+    @staticmethod
+    def print_bottom_5(frequent_separators_list):
+        print_string = []
+        counts = min(5, len(frequent_separators_list))
+        for i in range(-1, (-1 * counts) - 1, -1):
+            print_string.append("{}({:.2f}%)".format(frequent_separators_list[i][0],
+                                                     frequent_separators_list[i][1] * 100))
+        print("\tBottom {}: {}".format(counts, ", ".join(print_string)))
+
+    @staticmethod
+    def filter_by_frequency(frequent_separators_list, threshold):
+        return list(filter(lambda gram: gram[1] >= threshold, frequent_separators_list))
 
     @staticmethod
     def make_candidate_separators(frequent_separators):
