32d7e7ddf74a6556163694deff18293f92122aed
Jonghoon Won <baryberri@icloud.com>
2018-11-03 03:06:45 +0900

    Add timer to caffe_python

--- classifier/caffe_python/main.py
+++ classifier/caffe_python/main.py
@@ -1,4 +1,5 @@
 import caffe
+from timer import Timer
 from compute_bfd import compute_bfd
 from fragment import Fragment
 
@@ -13,18 +14,21 @@ def main():
                    4: "mp3",
                    5: "pdf",
                    6: "png"}
-    directories = ["/Users/barber/Data/fti_small_data/test_data/exe",
-                   "/Users/barber/Data/fti_small_data/test_data/html",
-                   "/Users/barber/Data/fti_small_data/test_data/hwp",
-                   "/Users/barber/Data/fti_small_data/test_data/jpg",
-                   "/Users/barber/Data/fti_small_data/test_data/mp3",
-                   "/Users/barber/Data/fti_small_data/test_data/pdf",
-                   "/Users/barber/Data/fti_small_data/test_data/png"]
+    directories = ["/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/exe",
+                   "/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/html",
+                   "/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/hwp",
+                   "/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/jpg",
+                   "/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/mp3",
+                   "/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/pdf",
+                   "/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/png"]
     fragment_getter = Fragment(num_fragments=2000, file_types=file_types, directories=directories, fragment_size=4096)
 
     # Prepare caffe network
     net = caffe.Net("./deploy.prototxt", "./model.caffemodel", caffe.TEST)
     net.blobs["data"].reshape(1, 1, 256, 1)
+
+    # Setup timer
+    timer = Timer(name="Elapsed Time")
 
     # Classify
     total_fragments = 0
@@ -33,10 +37,12 @@ def main():
     while fragment is not None:
         total_fragments += 1
 
+        timer.start()
         net.blobs["data"].data[...] = compute_bfd(fragment)
         result = net.forward()
         prob = result["prob"].tolist()[0]
         classified_type = file_groups[prob.index(max(prob))]
+        timer.stop()
 
         if classified_type == file_type:
             correct_fragments += 1
@@ -46,6 +52,7 @@ def main():
     print("Total Fragments: {}".format(total_fragments))
     print("Correct Classification: {} ({:.2f})%".format(correct_fragments,
                                                         correct_fragments / total_fragments * 100))
+    timer.print()
 
 
 if __name__ == "__main__":
