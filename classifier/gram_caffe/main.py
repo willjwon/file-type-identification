66b57b5043c4b8ad82f1274e6259cd9e134f8f5a
Jonghoon Won <baryberri@icloud.com>
2018-11-04 14:44:14 +0900

    Display detailed results

--- classifier/gram_caffe/main.py
+++ classifier/gram_caffe/main.py
@@ -22,10 +22,10 @@ def main():
                    "/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/mp3",
                    "/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/pdf",
                    "/home/jonghoon/Desktop/datasets/data/fti_small_data/test_data/png"]
-    fragment_getter = Fragment(num_fragments=2000, file_types=file_types, directories=directories, fragment_size=4096)
+    fragment_getter = Fragment(num_fragments=10000, file_types=file_types, directories=directories, fragment_size=1000)
 
     # Prepare gram network
-    gram_classifier = GramClassifier(fragment_size=4096)
+    gram_classifier = GramClassifier(fragment_size=1000)
 
     # Prepare caffe network
     net = caffe.Net("./deploy.prototxt", "./model.caffemodel", caffe.TEST)
@@ -35,13 +35,18 @@ def main():
     timer = Timer(name="Elapsed Time")
 
     # Classify
-    total_fragments = 0
-    classified_fragments = 0
-    correct_fragments = 0
+    gram_classified_fragments = 0
+    gram_correct_fragments = 0
+    nn_classified_fragments = 0
+    nn_correct_fragments = 0
+    classification_table = dict()
+    for type1 in file_types:
+        classification_table[type1] = dict()
+        for type2 in file_types:
+            classification_table[type1][type2] = 0
+
     fragment, file_type = fragment_getter.get_fragment()
     while fragment is not None:
-        total_fragments += 1
-
         timer.start()
         # Try gram classification first
         classified_type = gram_classifier.classify(fragment)
@@ -53,20 +58,41 @@ def main():
             prob = result["prob"].tolist()[0]
             classified_type = file_groups[prob.index(max(prob))]
             timer.stop()
+
+            nn_classified_fragments += 1
+            if file_type == classified_type:
+                nn_correct_fragments += 1
         else:
             timer.stop()
-            classified_fragments += 1
 
-        if classified_type == file_type:
-            correct_fragments += 1
+            gram_classified_fragments += 1
+            if file_type == classified_type:
+                gram_correct_fragments += 1
+
+        classification_table[file_type][classified_type] += 1
 
         fragment, file_type = fragment_getter.get_fragment()
 
-    print("Total Fragments: {}".format(total_fragments))
-    print("Classified Fragments: {} ({:.2f}%)".format(classified_fragments,
-                                                      classified_fragments / total_fragments * 100))
-    print("Correct Classification: {} ({:.2f})%".format(correct_fragments,
-                                                        correct_fragments / total_fragments * 100))
+    total_fragments = gram_classified_fragments + nn_classified_fragments
+    total_correct = gram_correct_fragments + nn_correct_fragments
+    print()
+    print("Gram: {}/{} ({:.2f}%), Accuracy: {:.2f}%".format(gram_classified_fragments,
+                                                            total_fragments,
+                                                            gram_classified_fragments / total_fragments * 100,
+                                                            gram_correct_fragments / gram_classified_fragments * 100))
+    print("MLP: {}/{} ({:.2f}%), Accuracy: {:.2f}%".format(nn_classified_fragments,
+                                                           total_fragments,
+                                                           nn_classified_fragments / total_fragments * 100,
+                                                           nn_correct_fragments / nn_classified_fragments * 100))
+    print("Total Accuracy: {:.2f}%".format(total_correct / total_fragments * 100))
+    print("Accuracy Table:")
+    print("\t" + "\t".join(file_types))
+    for type1 in file_types:
+        print("{}\t".format(type1), end="")
+        for type2 in file_types:
+            print("{:3.2f}%".format(classification_table[type1][type2] / 10000 * 100), end="\t")
+        print()
+
     timer.print()
 
 
