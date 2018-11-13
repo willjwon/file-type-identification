f1a50a2c1b37de84403701de3e8c9334adf14ee6
Jonghoon Won <baryberri@icloud.com>
2018-11-13 15:30:45 +0900

    Replace fragment.py to read whole file

--- classifier/file_caffe_python/fragment.py
+++ classifier/file_caffe_python/fragment.py
@@ -3,17 +3,13 @@ from print_progress import print_progress
 
 
 class Fragment:
-    def __init__(self, num_fragments, file_types, directories, fragment_size):
-        self.num_fragments = num_fragments
+    def __init__(self, file_types, directories):
         self.file_paths = dict()
         self.file_indices = dict()
         self.files = dict()
-        self.left_fragments = dict()
-        self.fragment_size = fragment_size
         self.file_types = file_types
         self.file_type_index = 0
         for file_type, directory in zip(file_types, directories):
-            self.left_fragments[file_type] = num_fragments
             self.file_paths[file_type] = \
                 [os.path.join(directory, filename) for filename in os.listdir(directory)
                  if ((not filename.startswith(".")) and os.path.isfile(os.path.join(directory, filename)))]
@@ -50,30 +46,11 @@ class Fragment:
         self.files[file_type].close()
         self.file_indices[file_type] = 0
         self.files[file_type] = open(self.file_paths[file_type][self.file_indices[file_type]], "rb")
-        self.left_fragments[file_type] = self.num_fragments
 
     def get_fragment(self):
         file_type = self.file_types[self.file_type_index]
 
-        if self.left_fragments[file_type] <= 0:
-            self.reset_type(file_type)
-            self.file_type_index += 1
-            if self.file_type_index >= len(self.file_types):
-                self.file_type_index = 0
-                return None, None
+        data = self.files[file_type].read()
+        open_success = self.open_next_file()
 
-            file_type = self.file_types[self.file_type_index]
-
-        data = self.files[file_type].read(self.fragment_size)
-        while len(data) < self.fragment_size:
-            open_success = self.open_next_file()
-            if not open_success[0]:
-                if not open_success[1]:
-                    return None, None
-                file_type = self.file_types[self.file_type_index]
-                data = self.files[file_type].read(self.fragment_size)
-            else:
-                data = self.files[file_type].read(self.fragment_size)
-
-        self.left_fragments[file_type] -= 1
-        return data, file_type
+        return data, file_type, open_success[1]
